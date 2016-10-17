ae2e076ffc9bbe7880599aa3621cc0ab367890bc
kevinpmcc <kevinpatrickmccarthy@gmail.com>
2016-10-17 13:19:13 +0100

    trying structs

--- app.rb
+++ app.rb
@@ -4,22 +4,30 @@ class DatabaseServer < Sinatra::Base
 
   enable :sessions
 
+  @data = []
+
   get '/set' do
     params.map {|k, v|
-      session["#{k}"] = v
+      Struct.new(key: k.to_s, value: v.to_s)
     }
     'stored key pair value in session'
   end
 
-
   get '/get' do
-    passed_key = params[:key]
-    session["#{passed_key}"]
+    ObjectSpace.each_object(Struct).each { |thing| 
+      puts "thing.key is" + thing.key
+      puts "thing.value is" + thing.value
+      puts "keyword is" + keyword
+          if thing.key.to_s == keyword
+            return_value = thing.value
+          end
+      }
+    return_value
+    #passed_key = params[:key]
+    #session["#{passed_key}"]
   end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
-
-
 
--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -1,16 +1,19 @@
 require 'spec_helper'
 
+set_url = '/set?mykey=myvalue'
+get_url = '/get?key=mykey'
+
 feature 'Accept key and value pair' do
   scenario 'key and value are sent through get request and receive confirmation have been saved' do
-    visit '/set?mykey=myvalue'
+    visit set_url
     expect(page).to have_content('stored key pair value in session')
   end
 end
 
 feature 'Return value after key and value have been added' do
   scenario 'once key and value have been entered then the value can be retrieved' do
-    visit '/set?mykey=myvalue'
-    visit '/get?key=mykey'
+    visit set_url
+    visit get_url
     expect(page).to have_content('myvalue')
   end
 end
