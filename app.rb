fc106aeac776dc1353355b0628b54fd54408ecdf
kevinpmcc <kevinpatrickmccarthy@gmail.com>
2016-10-17 13:56:10 +0100

    more noodling trying to add somethings to an array

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra/base'
+require_relative 'something'
 
 class DatabaseServer < Sinatra::Base
 
@@ -8,25 +9,29 @@ class DatabaseServer < Sinatra::Base
 
   get '/set' do
     params.map {|k, v|
-      Struct.new(key: k.to_s, value: v.to_s)
+      create_things(k.to_s, v.to_s)
     }
     'stored key pair value in session'
   end
 
   get '/get' do
-    ObjectSpace.each_object(Struct).each { |thing| 
-      puts "thing.key is" + thing.key
-      puts "thing.value is" + thing.value
-      puts "keyword is" + keyword
-          if thing.key.to_s == keyword
-            return_value = thing.value
-          end
-      }
+    keyword = params[:key]
+    return_value = ""
+    ObjectSpace.each_object(Something).each { |thing| 
+      #puts "thing.key is" + thing.key
+      #puts "thing.value is" + thing.value
+      #puts "keyword is" + keyword
+      if thing.key.to_s == keyword
+        return_value = thing.value
+      end
+    }
     return_value
-    #passed_key = params[:key]
-    #session["#{passed_key}"]
   end
 
+  def create_things(k, v)
+    thing = Something.new(key: k.to_s, value: v.to_s)
+    @data << thing
+  end
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
