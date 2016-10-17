6e66c2b19660c41e9215d53a8acb5a31fa222dc7
kevinpmcc <kevinpatrickmccarthy@gmail.com>
2016-10-17 12:18:37 +0100

    completed spiking, works, very messy, taking a break and returning to reassess

--- app.rb
+++ app.rb
@@ -2,32 +2,40 @@ require 'sinatra/base'
 require_relative 'something'
 
 class DatabaseServer < Sinatra::Base
+
   get '/' do
     'Hello DatabaseServer!'
   end
 
 
   get '/set' do
+    $array_of_things = []
     params.map {|k,v|
-        create_things(k.to_s, v.to_s)
+      create_things(k.to_s, v.to_s)
     }
+    "posted"
   end
 
-  get '/posted' do
-    "that's been posted"
-  end
 
   get '/get' do
-    #keyword = params[:key]
-   # @object[keyword] 
-    #puts @params[params[:key]]
+    return_value = ""
+    keyword = params[:key]
+    ObjectSpace.each_object(Something).each { |thing| 
+      puts "thing.key is" + thing.key
+      puts "thing.value is" + thing.value
+      puts "keyword is" + keyword
+          if thing.key.to_s == keyword
+            return_value = thing.value
+          end
+      }
+    puts return_value
+    return_value
   end
 
   
   def create_things(k,v)
-    @my_hash = {}
-    @my_hash[k] = v
-      
+    $array_of_things << Something.new(key: k, value: v)
+    
   end
 
   # start the server if ruby file executed directly
